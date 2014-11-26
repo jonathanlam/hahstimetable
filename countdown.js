49648250047ed031164ca0b131a2c49e6962ec58
George Trapezium <jonathanlam@users.noreply.github.com>
2014-11-27 10:58:12 +1100

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -182,7 +182,7 @@ BellEvent.prototype.getDate = function getDate() {
 
 	date.setHours(this.hour);
 	date.setMinutes(this.minute);
-	date.setSeconds(secsOffset-17);
+	date.setSeconds(secsOffset-18);
 	return date;
 };
 
